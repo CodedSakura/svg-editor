8d13b7e3ed77b9e9963da9638c15bd136b2e0336
codedsakura <sakuracoded@gmail.com>
2023-07-19 16:25:34 +0300

    add ability to select buttons

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,29 @@ enum DragModalState {
   RESIZE_BOTTOM = "ns-resize"
 }
 
+enum Tool {
+  CURSOR,
+  LINE,
+  ARC,
+  BEZIER,
+  MOVE,
+  ZOOM,
+}
+
+enum PropertyPanel {
+  NONE,
+  NEW,
+  PATHS,
+  PATH_PROPS,
+}
+
 export default function App() {
   const [ size, setSize ] = useState([ 16, 16 ]);
   const [ bottomHeight, setBottomHeight ] = useState(200);
   const [ dragModal, setDragModal ] = useState(DragModalState.OFF);
+  const [ lightMode, setLightMode ] = useState(false);
+  const [ tool, setTool ] = useState(Tool.CURSOR);
+  const [ propertyPanel, setPropertyPanel ] = useState(PropertyPanel.NONE);
 
   const dragModalMove = useCallback((e: React.MouseEvent) => {
     if (dragModal === DragModalState.OFF) {
@@ -43,21 +62,42 @@ export default function App() {
     </div>
 
     <div className="sidebar">
-      <div>A</div>
-      <div>B</div>
-      <div>C</div>
+      <div
+            className={tool === Tool.CURSOR ? "active" : undefined}
+            onClick={() => setTool(Tool.CURSOR)}>Cursor</div>
+      <hr />
+      <div
+            className={tool === Tool.LINE ? "active" : undefined}
+            onClick={() => setTool(Tool.LINE)}>Line</div>
+      <div
+            className={tool === Tool.ARC ? "active" : undefined}
+            onClick={() => setTool(Tool.ARC)}>Arc</div>
+      <div
+            className={tool === Tool.BEZIER ? "active" : undefined}
+            onClick={() => setTool(Tool.BEZIER)}>Bezier</div>
+      <hr />
+      <div
+            className={tool === Tool.MOVE ? "active" : undefined}
+            onClick={() => setTool(Tool.MOVE)}>Move</div>
+      <div
+            className={tool === Tool.ZOOM ? "active" : undefined}
+            onClick={() => setTool(Tool.ZOOM)}>Zoom</div>
     </div>
-    <svg width="100%" height="100%">
-      <pattern id="p1" x={-5} y={-5} viewBox="0 0 10 10" width={10} height={10} patternUnits="userSpaceOnUse">
-        <line x1={0} x2={10} stroke="#8888" strokeWidth={1} />
-        <line y1={0} y2={10} stroke="#8888" strokeWidth={1} />
+    <svg width="100%" height="100%" className={lightMode ? "light" : undefined}>
+      <pattern id="grid" x={-5} y={-5} viewBox="0 0 10 10" width={10} height={10} patternUnits="userSpaceOnUse" stroke="#8888">
+        <line x1={0} x2={10} strokeWidth={1} />
+        <line y1={0} y2={10} strokeWidth={1} />
       </pattern>
-      <rect fill="url(#p1)" width="200%" height="200%" />
+      <rect fill="url(#grid)" width="200%" height="200%" />
     </svg>
     <div className="sidebar">
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
+      <div>New</div>
+      <div>Paths</div>
+      <div>Path properties</div>
+      <hr />
+      <div
+            className={lightMode ? "active" : undefined}
+            onClick={() => setLightMode(v => !v)}>Light</div>
     </div>
     <div id="size-adjust" onMouseDown={() => setDragModal(DragModalState.RESIZE_BOTTOM)}></div>
     <div className="sidebar">
